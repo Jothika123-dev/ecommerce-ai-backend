015d8b68e132bd1fc1d3c75f8a1b25b8853b33a2
Nandhi2 <cln19724@gmail.com>
2026-08-31 19:51:52 +0530

    Update Flask app for Render

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask, jsonify
 from flask_cors import CORS
 
@@ -28,4 +30,8 @@ def health():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT", 5000)),
+        debug=False
+    )
